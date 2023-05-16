8814c4b39e0a68c578c35ec0e09ae7d7118e331d
Pablo Clemente <martin.clemente@cencosud.com>
2023-05-16 12:38:36 -0300

    New modified carousel started

--- src/components/atoms/carousel/carousel.styles.tsx
+++ src/components/atoms/carousel/carousel.styles.tsx
@@ -1,4 +1,62 @@
 import styled, { css } from 'styled-components';
 
+type ButtonProps = {
+    right?: boolean;
+};
+
+type DotProps = {
+    active?: boolean;
+};
+
 export const CarouselContainer = styled.div`
+    width: 100vw;
+    height: fit-content;
+    position: relative;
+`;
+
+export const CarouselImageContainer = styled.div`
+    width: 100%;
+    height: fit-content;
+    position: relative;
+
+    img{
+        width: 100%;
+        height: auto;
+    }
+`;
+
+export const CarouselNavButton = styled.button<ButtonProps>`
+    width: 35px;
+    height: 35px;
+    outline: none;
+    border: none;
+    background: none;
+    cursor: pointer;
+    padding: 0;
+    position: absolute;
+    top: 50%;
+    border-radius: 50%;
+    color: white;
+    box-shadow: 0px 4px 60px 20px rgba(3,3,3,0.9), inset 0 -3em 3em rgba(3,3,3,0.5);
+    transform: translate(0, -50%);
+    ${(props) => props.right === true ? css`right: 2%;` : css`left: 2%;`}
+`;
+
+export const CarouselDotContainer = styled.div`
+    display: flex;
+    gap: 5px;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: -3%;
+    left: 50%;
+    transform: translate(-50%, 0);
+`;
+
+export const CarouselDot = styled.div<DotProps>`
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    cursor: pointer;
+    ${(props) => props.active === true ? css`background-color:blue;` : css`background-color:red;`}
 `;
--- src/components/atoms/carousel/index.tsx
+++ src/components/atoms/carousel/index.tsx
@@ -1,58 +1,113 @@
-import React from 'react';
-import { CarouselProps } from './carousel.types';
-import Image from 'next/image';
-import { isMobile } from 'react-device-detect';
-import { CarouselContainer } from './carousel.styles';
-import { Carousel as CarouselComponent } from 'react-responsive-carousel'; //TODO: ver
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; //TODO: requires a loader
+import { useState } from "react";
+import { CarouselContainer, CarouselDot, CarouselDotContainer, CarouselImageContainer, CarouselNavButton } from "./carousel.styles";
+import { CarouselProps } from "./carousel.types";
+import Image from "next/image";
 
-const Carousel = (props: CarouselProps) => {
+
+const Carousel = ( props : CarouselProps) => {
+
   const { items, itemsPerRow = 1, styles } = props;
 
-  const getCenterSlidePercentage = (total: number, countElements: number) => {
-    if (countElements === 1) {
-      return 100;
-    } else return 100 / countElements;
-  };
+  const [ imageIndex, setImageIndex ] = useState(0);
+
+  const HandleNext = () =>{
+    setImageIndex(state => state += 1);
+    if(imageIndex === items.length -1) setImageIndex(0);
+  }
+
+  const HandlePrev = () =>{
+    setImageIndex(state => state -= 1);
+    if(imageIndex === 0) setImageIndex(items.length -1);
+  }
 
   return (
-    <React.Fragment>
-      <CarouselContainer style={styles}>
-        <CarouselComponent
-          showArrows
-          showThumbs={false}
-          centerMode={items.length !== 1}
-          showIndicators={false}
-          showStatus={items.length === 1}
-          centerSlidePercentage={getCenterSlidePercentage(
-            items.length,
-            itemsPerRow
-          )}
-          selectedItem={0}
-        >
-          {items.map((itemCarousel, index) => {
-            const {
-              image = '',
-              mobileImage,
-              altDescription,
-              link,
-            } = itemCarousel;
-            return (
-              <div className='px-1' key={`carousel_item_${index}`}>
-                <Image
-                  priority={true}
-                  src={isMobile && mobileImage ? mobileImage : image}
-                  alt={altDescription || ''}
-                  width={925}
-                  height={271}
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-            );
-          })}
-        </CarouselComponent>
-      </CarouselContainer>
-    </React.Fragment>
+    <CarouselContainer>
+      <CarouselImageContainer>
+        <Image 
+          src={items[imageIndex].image}
+          width={100}
+          height={100}
+          sizes="100vw"
+          alt="img"
+        />
+      </CarouselImageContainer>
+
+      <CarouselNavButton right onClick={HandleNext}>
+        <div> go </div>
+      </CarouselNavButton>
+      <CarouselNavButton onClick={HandlePrev}>
+        <div> back </div>
+      </CarouselNavButton>
+
+      <CarouselDotContainer>
+        {items.map ((dot, index) =>(
+          <CarouselDot key={dot.title} active={index === imageIndex}/>
+        )) }
+      </CarouselDotContainer>
+
+    </CarouselContainer>
   );
 };
+
 export default Carousel;
+
+
+
+// import React from 'react';
+// import { CarouselProps } from './carousel.types';
+// import Image from 'next/image';
+// import { isMobile } from 'react-device-detect';
+// import { CarouselContainer } from './carousel.styles';
+// import { Carousel as CarouselComponent } from 'react-responsive-carousel'; //TODO: ver
+// import 'react-responsive-carousel/lib/styles/carousel.min.css'; //TODO: requires a loader
+
+// const Carousel = (props: CarouselProps) => {
+//   const { items, itemsPerRow = 1, styles } = props;
+
+//   const getCenterSlidePercentage = (total: number, countElements: number) => {
+//     if (countElements === 1) {
+//       return 100;
+//     } else return 100 / countElements;
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <CarouselContainer style={styles}>
+//         <CarouselComponent
+//           showArrows
+//           showThumbs={false}
+//           centerMode={items.length !== 1}
+//           showIndicators={false}
+//           showStatus={items.length === 1}
+//           centerSlidePercentage={getCenterSlidePercentage(
+//             items.length,
+//             itemsPerRow
+//           )}
+//           selectedItem={0}
+//         >
+//           {items.map((itemCarousel, index) => {
+//             const {
+//               image = '',
+//               mobileImage,
+//               altDescription,
+//               link,
+//             } = itemCarousel;
+//             return (
+//               <div className='px-1' key={`carousel_item_${index}`}>
+//                 <Image
+//                   priority={true}
+//                   src={isMobile && mobileImage ? mobileImage : image}
+//                   alt={altDescription || ''}
+//                   width={925}
+//                   height={271}
+//                   style={{ width: '100%', height: 'auto' }}
+//                 />
+//               </div>
+//             );
+//           })}
+//         </CarouselComponent>
+//       </CarouselContainer>
+//     </React.Fragment>
+//   );
+// };
+// export default Carousel;
