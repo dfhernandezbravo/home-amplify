c2d1c36a3c9de0b39c0904f5a4d90bba2005d2ef
Pablo Clemente <martin.clemente@cencosud.com>
2023-05-31 10:10:30 -0300

    minor writing changes

--- src/components/atoms/textfield/textfield.types.ts
+++ src/components/atoms/textfield/textfield.types.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, InputHTMLAttributes } from "react";
+import { ChangeEvent, InputHTMLAttributes } from 'react';
 
 export interface TextfieldProps extends InputHTMLAttributes<HTMLInputElement> {
   type: string;
