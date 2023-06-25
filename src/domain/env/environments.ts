938f94bd61e423d454fb913591914c2aead561a7
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2023-06-25 02:05:57 -0300

    add get to content only from home

--- src/domain/env/environments.ts
+++ src/domain/env/environments.ts
@@ -5,13 +5,19 @@ const env = {
 };
 export const environments = () => {
   let hostURI;
+  let cmsURI;
+  let cmsAPIKEY;
   switch (process.env.NEXT_PUBLIC_ENV) {
     case env.DEVELOPMENT:
     case env.STAGING:
-      hostURI = process.env.NEXT_PUBLIC_CMS_STG;
+      hostURI = process.env.NEXT_PUBLIC_HOST_STG;
+      cmsURI = process.env.NEXT_PUBLIC_CMS_URL_STG;
+      cmsAPIKEY = process.env.NEXT_PUBLIC_CMS_API_KEY;
       break;
   }
   return {
-    hostURI
+    hostURI,
+    cmsURI,
+    cmsAPIKEY
   };
 };
