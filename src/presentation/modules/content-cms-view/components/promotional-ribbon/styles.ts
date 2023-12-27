467d56fba83db1d30aeaf473c30432d6204866be
Pablo Clemente <martin.clemente@cencosud.com>
2023-12-27 09:55:27 -0300

    fix: margin and width on atom ribbon

--- src/presentation/modules/content-cms-view/components/promotional-ribbon/styles.ts
+++ src/presentation/modules/content-cms-view/components/promotional-ribbon/styles.ts
@@ -4,19 +4,23 @@ import styled from 'styled-components';
 
 // -> remover marin-bottom -4px;
 
-export const Container = styled(Link)<{ background: string }>`
-  width: 100%;
-  height: fit-content;
+export const Container = styled(Link)<{
+  background: string;
+  fullWidth: boolean;
+}>`
+  width: ${(props) => (props.fullWidth ? '100%' : '77.25rem')};
+  margin: 0 auto;
+  height: auto;
   min-height: 40px;
+  max-height: 200px;
   cursor: pointer;
   display: flex;
   justify-content: center;
   background-color: ${(props) => props.background};
-  margin: 0;
 `;
 
-export const ImageRibbon = styled(Image)<{ fullWidth: boolean }>`
-  width: ${(props) => (props.fullWidth ? '100%' : 'fit-content')};
+export const ImageRibbon = styled(Image)`
+  width: 100%;
   position: relative;
   height: auto;
   min-height: 40px;
